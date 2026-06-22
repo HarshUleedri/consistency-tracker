8e4e86a814ab5e3836986443b16d8ce7934e659b
HarshUleedri <ulidari2@gmail.com>
2026-06-22 13:05:32 +0530

    feat: prisma orm and better auth with signin and signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const user = session?.user;
+
+  if(!user) {
+    redirect('/signin')
+  }
+
   return (
     <div className="text-5xl">
       Home
