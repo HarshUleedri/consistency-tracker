6a48e3c3ae647bbd04cb42b02a69a68053fe0104
HarshUleedri <ulidari2@gmail.com>
2026-06-27 08:46:04 +0530

    feat: dashboard greeting created

---
+++ src/app/(protected)/components/DashboardGreetings.tsx
@@ -0,0 +1,23 @@
+export default function DashboardGreetings() {
+  const currentHour = new Date().getHours();
+
+  return (
+    <div className="space-y-2">
+      <p className="text-xs font-mono font-extrabold   ">
+        {new Date().toDateString()}
+      </p>
+      <h3>
+        {currentHour < 12
+          ? "Good Morning"
+          : currentHour < 18
+            ? "Good Afternoo"
+            : "Good Evening"}
+        <span className="">👋</span>
+      </h3>
+
+      <p className="font-mono text-sm font-extralight">
+     Consistancy is the key to master anything.
+      </p>
+    </div>
+  );
+}
--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,3 +1,9 @@
+import DashboardGreetings from "./components/DashboardGreetings";
+
 export default async function Home() {
-  return <div className="text-sm">Home</div>;
+  return (
+    <div className="p-4 sm:p-8">
+      <DashboardGreetings />
+    </div>
+  );
 }
